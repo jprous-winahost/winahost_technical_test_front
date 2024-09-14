1a410fdf339be24914f1da18058bb4c42e084899
jprous <jprous@winahost.com>
2024-09-14 09:36:16 +0200

    include typescript to project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import App from "./App";
 
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>¡Hola, mundo!</h1>);
+root.render(<App/>);
